72a35b2e17c070dd5dd3b2680b337b3df22a35d8
Franklyn Tackitt <franklyn@tackitt.net>
2017-02-19 17:18:19 -0700

    Add basic Attract mode

--- toddler/mainloop.py
+++ toddler/mainloop.py
@@ -12,13 +12,14 @@ import pygame.time
 
 from toddler.colors import BLACK, WHITE
 from toddler.drawable import DrawableList
+from toddler.shapes import Firework
 from toddler.shapes import random_shape
 from toddler.shapes.letter import Letter
 
 
-class Mode(Enum):
+class GameMode(Enum):
     ATTRACT = 1
-    PLAY = 2
+    ACTIVE = 2
 
 
 class GameState(object):
@@ -28,6 +29,9 @@ class GameState(object):
     drawables: DrawableList
     running: bool
     desired_framerate: int = 60
+    game_mode: GameMode
+    last_keypress: int = 0
+    last_generate: int = 0
 
     def __init__(self):
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.HWSURFACE, 32)
@@ -36,11 +40,17 @@ class GameState(object):
         self.clock = pygame.time.Clock()
         self.font16 = pygame.font.SysFont('Calibri', 16)
 
+        self.game_mode = GameMode.ATTRACT
+
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.running = False
             elif event.type == pygame.KEYDOWN:
+                if self.game_mode == GameMode.ATTRACT:
+                    self.game_mode = GameMode.ACTIVE
+                    self.last_keypress = 0
+
                 if pygame.key.name(event.key) in string.ascii_letters + string.digits:
                     self.drawables.append(Letter.new_random(self.screen, pygame.key.name(event.key)))
                 else:
@@ -48,6 +58,17 @@ class GameState(object):
 
     def tick(self):
         t = self.clock.get_time()
+        self.last_keypress += t
+
+        if self.last_keypress > 5 * 1000:
+            self.game_mode = GameMode.ATTRACT
+
+        if self.game_mode == GameMode.ATTRACT:
+            self.last_generate += t
+            if self.last_generate > 500:
+                self.drawables.append(Firework.new_random(self.screen, self.drawables))
+                self.last_generate = 0
+
         for drawable in list(self.drawables):
             if not drawable.tick(t, self.screen):
                 self.drawables.remove(drawable)
@@ -74,6 +95,7 @@ class GameState(object):
             self.tick()
             self.draw_start()
             self.blit()
+            self.draw_fps()
             self.draw_flip()
 
         pygame.quit()
